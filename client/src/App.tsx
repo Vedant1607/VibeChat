77cf8441f4a627615b7c00a5d8e96f7c9e9f9f40
Vedant Sinha <sinhavedant0705@gmail.com>
2025-11-17 23:46:29 +0530

    feat: add route paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
-  return <></>;
+  return (
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
